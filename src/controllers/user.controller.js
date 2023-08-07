b35d52e366deed83a8cb633e726b6549bb438e47
rafael-isidro <rafa-isidro@hotmail.com>
2023-08-07 14:02:01 -0300

    commit teste

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,11 +2,11 @@ const create = (req, res) => {
   const {name, username, email, password, avatar, background} = req.body;
 
   if (!name || !username || !email || !password || !avatar || !background) {
-    res.status(400).send({message: "Submit all fields for registration"});
+    res.status(400).send({message: "Submit all fields for registration."});
   };
   
   res.status(201).send({
-    message: "User created succesfully",
+    message: "User created succesfully.",
     user: {
       name,
       username,
